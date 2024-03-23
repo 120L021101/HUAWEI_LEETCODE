decf0fb63d11f2c98d4ed3f642bdb4ba5d1b762d
Zu 祖 <1352458258@qq.com>
2024-03-23 19:20:31 +0800

    加油站

--- 134.java
+++ 134.java
@@ -1,17 +1,14 @@
 class Solution {
-    private static int count = 0;
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        count += 1;
         long acc = 0;
         long minAcc = 0; int idx = 0;
         for (int i = 0; i < gas.length; ++i) {
             acc += gas[i] - cost[i];
-            if (i == 0 || acc < minAcc) {
+            if (i == 0 || acc <= minAcc) {
                 idx = i + 1;
                 minAcc = acc;
             }
         }
-        if (count == 40) return 0;
         if (acc >= 0) {
             return idx == gas.length ? 0 : idx;
         }
