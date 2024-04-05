c44d417d62a83635652ac1c8bf874d3bfe6eb5c9
Zu 祖 <1352458258@qq.com>
2024-04-05 14:20:05 +0800

    小红关鸡

---
+++ NC269221.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+using lli = long long int;
+
+int main(void) {
+    lli n, k;
+    cin >> n >> k;
+    lli pos[n];
+    for (lli i = 0; i != n; ++i)
+        cin >> pos[i];
+    sort(pos, pos + n);
+    lli cWin = 0;
+    lli l = 0, r = 0;
+    lli maxCh = 0;
+    while (1) {
+        r ++;
+        if (r == n) break;
+        cWin = pos[r] - pos[l];
+        while ((cWin = pos[r] - pos[l]) > k) l ++;
+        maxCh = fmax(maxCh, r - l + 1);
+    }
+    cout << double(maxCh) / n << endl;
+    return 0;
+}
