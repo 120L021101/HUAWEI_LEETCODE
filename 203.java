9c9ddd1c399c7bbbacaa55559ff9399f87a836c2
Zu 祖 <1352458258@qq.com>
2024-04-10 22:51:36 +0800

    移除链表元素

---
+++ 203.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode header = new ListNode(0, head);
+        ListNode pointer = header;
+        while (pointer.next != null) {
+            if (pointer.next.val == val) {
+                pointer.next = pointer.next.next;
+            } else {
+                pointer = pointer.next;
+            }
+        }
+        return header.next;
+    }
+}
