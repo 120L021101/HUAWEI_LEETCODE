86cb06dc15402939c60f56e91ae6e616b973dcc4
Zu 祖 <1352458258@qq.com>
2024-04-10 23:22:57 +0800

    第一个错误的版本

---
+++ 278.java
@@ -0,0 +1,17 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+      public class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            long l = 1, r = n;
+            while (l < r) {
+                long mid = (l + r) / 2;
+                if (isBadVersion((int)mid)) {
+                    r = mid;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            return (int)l;
+        }
+    }
